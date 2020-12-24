b4472cf2d123cd6bb084e5946926cdf2c40257e3
Cassi <od18309@bristol.ac.uk>
2020-12-24 18:13:00 +0000

    can get the description of a video

--- yt-testing.py
+++ yt-testing.py
@@ -8,9 +8,16 @@ ytService = build('youtube', 'v3',developerKey=yt_key)
 request = ytService.search().list(
     part="snippet",
     maxResults=2,
-    q="ateez",
+    q="melodic dubstep mix",
     type="video"
 )
 
 response = request.execute()
 print(response['items'][0]['id']['videoId'])
+request2 = ytService.videos().list(
+    part="snippet",
+    id = response['items'][0]['id']['videoId']
+)
+
+response2 = request2.execute()
+print(response2['items'][0]['snippet']['description'])
